a620a2dbcaf1021680acea3e430505faade4a773
Priyank Bhikaji Naik <104668352+NAIKPRIYANK@users.noreply.github.com>
2023-08-22 00:10:00 +0530

    Delete NegativeAmount.java

--- NegativeAmount.java
+++
@@ -1,8 +0,0 @@
-package bankaccountapplication;
-
-public class NegativeAmount extends Exception {
-
-	public NegativeAmount() {
-        super("Invalid amount --> amount cannot be negative");
-    }
-}
