5756dd221fce147035e5ba778db6dd1e924aae70
Priyank Bhikaji Naik <104668352+NAIKPRIYANK@users.noreply.github.com>
2023-08-22 00:08:29 +0530

    Delete LowBalanceException.java

--- LowBalanceException.java
+++
@@ -1,8 +0,0 @@
-package bankaccountapplication;
-
-public class LowBalanceException extends Exception {
-
-	public LowBalanceException() {
-        super("Low balance: balance is below the minimum requirement");
-    }
-}
