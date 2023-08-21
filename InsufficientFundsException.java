ea01b65fe96d340ab77e2c3da77b8bec5f5a83c3
Priyank Bhikaji Naik <104668352+NAIKPRIYANK@users.noreply.github.com>
2023-08-22 00:08:19 +0530

    Delete InsufficientFundsException.java

--- InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package bankaccountapplication;
-public class InsufficientFundsException extends Exception{
-
-	public InsufficientFundsException() {
-        super("Insufficient funds: minimum balance requirement not met");
-    }
-}
